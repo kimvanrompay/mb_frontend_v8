c535a3cffef0ddaee59cc834a94fdfcda454d4c7
kimvanrompay <kimvanrompay@icloud.com>
2026-01-31 02:42:19 +0700

    Use Cloudinary logo in sidebar and remove navbar logo

--- src/app/components/layout/navbar-centered/navbar-centered.component.ts
+++ src/app/components/layout/navbar-centered/navbar-centered.component.ts
@@ -21,16 +21,6 @@ import { filter, map } from 'rxjs/operators';
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
              </button>
-
-             <div class="flex flex-col">
-                <div class="flex items-center text-xs font-mono text-gray-500 gap-2">
-                    <img src="assets/meribas-logo.png" alt="Meribas" class="h-5 w-auto">
-                    <span *ngIf="currentPage" class="flex items-center gap-2">
-                        <span>/</span>
-                        <span class="text-black font-medium">{{ currentPage }}</span>
-                    </span>
-                </div>
-             </div>
           </div>
 
           <!-- User Profile (Right) -->
